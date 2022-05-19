c8bd95ad52dfe0c2c6cceb4f2373d86f254f9031
Rainer Mayrhofer <100029587+StainXrm@users.noreply.github.com>
2022-05-19 09:05:21 +0200

    some thoughts and basic test

---
+++ js/easyselect.js
@@ -0,0 +1,42 @@
+window.X = (arg) => {
+    let selects = document.querySelectorAll(arg)
+    // let selectArr = Array.from(selects);
+    return new DOMNodes(selects);
+}
+
+class DOMNodes {
+    constructor(htmlArr) {
+        this.htmlArr = htmlArr;
+    }
+
+    css(option, set) {
+        if (!option) return this;
+        if (!set) {
+            this.htmlArr.forEach(function (element) {
+                let style = getComputedStyle(element).getPropertyValue(option);
+                console.log(style);
+            })
+            return this
+        }
+        this.htmlArr.forEach(function (element) {
+            element.style[option] = set;
+        })
+    }
+
+    replace(arg) {
+        if (!arg) return this;
+        this.htmlArr.forEach(function (element) {
+            element.innerHTML = arg;
+        })
+        return this;
+    }
+
+
+
+    empty() {
+        this.htmlArr.forEach(function (element) {
+            element.innerHTML = "";
+        })
+        return this;
+    }
+}
